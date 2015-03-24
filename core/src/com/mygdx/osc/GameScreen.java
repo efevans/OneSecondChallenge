7d418aa3b4fdfaf0413d012e1270a3254e6ecf5e
Eric Evans <efevans@ucsc.edu>
2015-03-24 16:24:27 -0700

    Added backgrounds for player feedback

--- core/src/com/mygdx/osc/GameScreen.java
+++ core/src/com/mygdx/osc/GameScreen.java
@@ -8,13 +8,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
 	
+	enum WinState
+	{
+		TIMING,
+		WONLAST,
+		LOSTLAST,
+		NOTPLAYED
+	}
+	
 	private OneSecondChallenge game;
 	float touchTimer;
+	boolean timing;
+	WinState winState;
 	
 	public GameScreen(OneSecondChallenge game){
 		this.game = game;
-		touchTimer = 0.0f;
-		System.out.println("hi");
+		this.touchTimer = 0.0f;
+		this.timing = false;
+		this.winState = WinState.NOTPLAYED;
 	}
 
 	@Override
@@ -26,17 +37,74 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		
+		// while screen is touched accumulate time of touch
 		if (Gdx.input.isTouched())
 		{
+			timing = true;
 			touchTimer += delta;
+			this.winState = WinState.TIMING;
+		}
+		else // screen is not being touched
+		{
+			
+			// if this is the first loop after finishing touching, check for win
+			if(timing)
+			{
+				if (withinBounds(touchTimer))
+				{
+					this.winState = WinState.WONLAST;
+				}
+				else
+				{
+					this.winState = WinState.LOSTLAST;
+				}
+				
+				timing = false;
+			}
+			
+			// logging the end time for testing, also reset timer here
+			if (touchTimer > 0.0f)
+			{
+				Gdx.app.log("MyTag", Float.toString(touchTimer));
+				touchTimer = 0.0f;
+			}
 		}
 		
-		if (!Gdx.input.isTouched() && touchTimer > 0.0)
+		setDisplay(this.winState);
+	}
+	
+	// checks if the 
+	private boolean withinBounds(float time)
+	{
+		if (time > 0.93f && time < 1.07f)
 		{
-			Gdx.app.log("MyTag", Float.toString(touchTimer));
-			touchTimer = 0.0f;
+			return true;
 		}
-
+		
+		return false;
+	}
+	
+	private void setDisplay(WinState winState)
+	{
+		switch (winState)
+		{
+		case TIMING:
+			Gdx.gl.glClearColor(0, 0, 1.0f, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			break;
+		case WONLAST:
+			Gdx.gl.glClearColor(0, 1.0f, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			break;
+		case LOSTLAST:
+			Gdx.gl.glClearColor(1.0f, 0, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			break;
+		case NOTPLAYED:
+			Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			break;
+		}
 	}
 
 	@Override
