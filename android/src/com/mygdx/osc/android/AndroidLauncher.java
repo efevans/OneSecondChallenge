96195c478776fbb2bb9268dfd277dfe2a815bbe1
Eric Evans <efevans@ucsc.edu>
2015-03-24 12:19:03 -0700

    initial libGDX game contents

---
+++ android/src/com/mygdx/osc/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.osc.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.osc.OneSecondChallenge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OneSecondChallenge(), config);
+	}
+}
